fd2d302f31c51c0d92cd2ae9496cdd41b23339f1
Minh1509 <nguyenquangminh15092003@gmail.com>
2023-08-30 16:00:27 +0700

    AC

--- Bien_va_kieu_du_lieu/PY01063_dem_so_trong_xau.py
+++ Bien_va_kieu_du_lieu/PY01063_dem_so_trong_xau.py
@@ -1,8 +1,14 @@
 t = int(input())
-for i in range(t) :
+for _ in range(t) :
     s = input()
     s1 = input()
     i =0
     dem =0
-    while i < len(s) - 3 :
-        s1 = s
+    while i < len(s)  :
+        tmp = s.find(s1, i, len(s))
+        if tmp != -1 :
+            dem += 1
+            i = tmp+  len(s1)
+        else :
+            break
+    print(dem)
