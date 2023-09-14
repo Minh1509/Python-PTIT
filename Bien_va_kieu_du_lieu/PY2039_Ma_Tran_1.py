b2317867b6ee885360dec759eeae6da7c058388a
Minh1509 <nguyenquangminh15092003@gmail.com>
2023-09-14 21:43:32 +0700

    AC

--- Bien_va_kieu_du_lieu/PY2039_Ma_Tran_1.py
+++ Bien_va_kieu_du_lieu/PY2039_Ma_Tran_1.py
@@ -4,3 +4,19 @@ a = [[]]*n
 for i in range(n):
     a[i] = [int(x) for x in input().split()]
 
+k = int(input())
+sum1 =0
+sum2 =0
+for i in range(n):
+    for j in range(n) :
+        if j > i:
+           sum1 += a[i][j]
+        elif i > j :
+            sum2 += a[i][j]
+tmp = abs(sum1- sum2)
+if tmp <= k:
+    print("YES")
+else :
+    print("NO")
+print(tmp)
+
